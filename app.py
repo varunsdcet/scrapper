0c1495335fcbb6843ac956703593a3e172ed6519
BABASHYAM <babashyam@GANESHs-MacBook-Pro.local>
2025-05-17 18:25:03 +0530

    kk

--- app.py
+++ app.py
@@ -228,4 +228,4 @@ def send_email_route():
     return render_template('index.html', results=None, message=message)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+   app.run(host='0.0.0.0', port=3000, debug=True)
